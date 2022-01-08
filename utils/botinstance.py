6adde0f6b712996f289485fe99b00e01c4df1a89
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-01-08 23:14:09 +0000

    Create botinstance.py

---
+++ utils/botinstance.py
@@ -0,0 +1,19 @@
+from discord import AllowedMentions, Intents
+from discord.ext import commands
+
+class Bot(commands.Bot):
+    def __init__(self):
+        super().__init__(
+                command_prefix=["c.", "C."],
+                case_insensitive=True,
+                help_command=commands.MinimalHelpCommand(),
+                allowed_mentions=AllowedMentions(
+                    users=True,
+                    everyone=False,
+                    roles=False,
+                    replied_user=False
+                ),
+                intents=Intents.all(),
+                owner_id=600056626749112322
+            )
+
