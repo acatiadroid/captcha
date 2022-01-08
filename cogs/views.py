3ea0a76a0e5c8b52d09a10627f2569c0a9e1611d
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-01-08 00:46:21 +0000

    Update views.py

--- cogs/views.py
+++ cogs/views.py
@@ -1,5 +1,9 @@
 import discord
+import asyncio
 from discord.ext.commands import CooldownMapping, CommandError
+
+from utils import images
+from cogs.captcha import Captcha
 
 def key(interaction):
     return interaction.user
@@ -14,31 +18,22 @@ class RegenCaptcha(discord.ui.View):
         self.timeout = 45
         self.ctx = None
 
-    @discord.ui.button(style=discord.ButtonStyle.blurple, label="Don't know? Regenerate image")
-    async def btnRegen(self, button: discord.ui.Button, interaction: discord.Interaction):
-        # TODO
-        pass
-
-class BeginVerification(discord.ui.View):
-    def __init__(self):
-        super().__init__()
-        self.timeout = 45
-        self.ctx = None
-        self.cooldown = CooldownMapping.from_cooldown(1, 30, key)
-
-
     async def interaction_check(self, interaction: discord.Interaction):
         retry_after = self.cooldown.update_rate_limit(interaction)
-        
         if retry_after:
-            return await interaction.response.send_message("You're on cooldown")
-    
+            raise ButtonOnCooldown(retry_after)
+
     async def on_error(self, error: Exception, item: discord.ui.Item, interaction: discord.Interaction):
         if isinstance(error, ButtonOnCooldown):
             time = f"{error.retry_after} second" if error.retry_after == 1 else f"{error.retry_after} seconds"
             return await interaction.response.send_message(f"You're on cooldown. Please wait {time}.")
 
-    @discord.ui.button(style=discord.ButtonStyle.green, label="Begin Verification")
-    async def btnBegin(self, button: discord.ui.Button, interaction: discord.Interaction):
-        # TODO
-        pass
+    @discord.ui.button(style=discord.ButtonStyle.blurple, label="Don't know? Regenerate image")
+    async def btnRegen(self, button: discord.ui.Button, interaction: discord.Interaction):
+        captcha = images.generate_captcha()
+        await interaction.response.send_message(
+            content="Complete this new captcha to gain access to the server:",
+            captcha=captcha["file_src"]
+        ) 
+
+
