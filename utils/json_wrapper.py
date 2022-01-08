80ed1ef7fc1ca5c987dabed12c086c1446857fb8
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-01-08 23:13:58 +0000

    Add get_all_data function

--- utils/json_wrapper.py
+++ utils/json_wrapper.py
@@ -8,3 +8,7 @@ def insert_config(data: dict, ctx):
 
     with open("config.json", "w") as file:
         json.dump(decoded, file, indent=4)
+
+def get_all_data():
+    with open("config.json") as file:
+        return json.load(file)
