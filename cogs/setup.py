4a2a6777e2b9fca321beee2db6ae19bb122edbfa
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-01-08 00:46:15 +0000

    Add some setup commands

---
+++ cogs/setup.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+
+from utils import checks
+from utils.json_wrapper import insert_config
+from cogs.views import BeginVerification
+
+class Configuration(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(name="create")
+    @checks.guild_manager()
+    async def _create(self, ctx, channel: discord.TextChannel, *, message = None):
+        """
+        Creates a message containing a button which will initialize the verification process.
+
+        Required permission:
+        Manage Guild
+
+        Arguments:
+            channel — the channel you want the message to get sent in.
+            message (OPTIONAL) — the message you want above the button. I.E "click below to verify"
+        """
+        e = discord.Embed(
+            color=0xfc433a,
+            title="Captcha",
+            description=f":lock: {message}" if message else ":lock: Please click the button below to begin a captcha to verify that you're human!"
+        )
+        e.set_footer(text="This server is protected from raids by Captcha Bot")
+        view = BeginVerification()
+        msg = await channel.send(embed=e, view=view)
+        insert_config(
+            {
+                "msg_channel": channel.id,
+                "msg_id": msg.id
+            },
+            ctx
+        )
+        
+        await ctx.send(f"Success! The message has been sent into {channel.mention}.")
+
+    
+
+def setup(bot):
+    bot.add_cog(Configuration(bot))
