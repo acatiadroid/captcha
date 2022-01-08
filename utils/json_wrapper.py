30598a1a9d3f6004b41e33ffddfd246fab3ec741
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-01-08 00:46:46 +0000

    Create json_wrapper.py

---
+++ utils/json_wrapper.py
@@ -0,0 +1,10 @@
+import json
+
+def insert_config(data: dict, ctx):
+    with open("config.json") as file:
+        decoded = json.load(file)
+    
+    decoded[ctx.guild.id] = data
+
+    with open("config.json", "w") as file:
+        json.dump(decoded, file, indent=4)
