bdfa7f98215ccaa2addaeb5c57ba6b90fc10c0b5
Luke <69216256+acatiadroid@users.noreply.github.com>
2022-01-08 23:15:14 +0000

    Update captcha.py

--- cogs/captcha.py
+++ cogs/captcha.py
@@ -2,12 +2,13 @@ import discord
 import asyncio
 from discord.ext import commands
 from discord.ext.commands import CooldownMapping, CommandError
+
 from cogs.views import RegenCaptcha
 from utils import images
+from utils.botinstance import Bot
 
 def key(interaction):
     return interaction.user
-
 
 class ButtonOnCooldown(CommandError):
     def __init__(self, retry_after):
@@ -18,29 +19,38 @@ class Captcha(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         global botobj
-        botobj: commands.Bot = self.bot
+        botobj = self.bot
     
-    class BeginVerification(discord.ui.View):
+    @commands.command(name="test")
+    @commands.is_owner()
+    async def _test(self, ctx):
+        await ctx.send('yo')
 
-        def __init__(self):
-            super().__init__()
-            self.timeout = None
-            self.ctx = None
-            self.cooldown = CooldownMapping.from_cooldown(1, 30, key)
+class BeginVerification(discord.ui.View):
+    def __init__(self):
+        super().__init__()
+        self.timeout = None
+        self.bot = botobj
+        self.cooldown = CooldownMapping.from_cooldown(1, 30, key)
 
-        async def interaction_check(self, interaction: discord.Interaction):
-            retry_after = self.cooldown.update_rate_limit(interaction)
+    # async def interaction_check(self, interaction: discord.Interaction):
+    #     print('yes')
+    #     retry_after = self.cooldown.update_rate_limit(interaction)
 
-            if retry_after:
-                raise ButtonOnCooldown(retry_after)
+    #     if retry_after:
+    #         raise ButtonOnCooldown(retry_after)
 
-        async def on_error(self, error: Exception, item: discord.ui.Item, interaction: discord.Interaction):
-            if isinstance(error, ButtonOnCooldown):
-                time = f"{error.retry_after} second" if error.retry_after == 1 else f"{error.retry_after} seconds"
-                return await interaction.response.send_message(f"You're on cooldown. Please wait {time}.")
 
-        @discord.ui.button(style=discord.ButtonStyle.green, label="Begin Verification", custom_id="begin_verification")
-        async def btnBegin(self, button: discord.ui.Button, interaction: discord.Interaction):
+    async def on_error(self, error: Exception, item: discord.ui.Item, interaction: discord.Interaction):
+        if isinstance(error, ButtonOnCooldown):
+            time = f"{error.retry_after} second" if error.retry_after == 1 else f"{error.retry_after} seconds"
+            return await interaction.response.send_message(f"You're on cooldown. Please wait {time}.", ephemeral=True)
+
+    @discord.ui.button(style=discord.ButtonStyle.green, label="Begin Verification", custom_id="btn")
+    async def btnBegin(self, button: discord.ui.Button, interaction: discord.Interaction):
+        try:
+            bot = Captcha(Bot())
+
             user = interaction.user
             captcha = images.generate_captcha()
             try:
@@ -55,7 +65,8 @@ class Captcha(commands.Cog):
             )
             e.add_field(name="Don't know the letters?", value="That's fine! Use the button below to generate a new captcha.")
             e.set_footer(text="You have 45 seconds to respond.")
-            await dm.send(embed=e, view=RegenCaptcha())
+            e.set_image(url=f"attachment://captcha.png")
+            await dm.send(embed=e, view=RegenCaptcha(), file=captcha["file_src"])
 
             await interaction.response.send_message(f":mailbox: You have mail! Check your DMs.", ephemeral=True)
 
@@ -63,7 +74,8 @@ class Captcha(commands.Cog):
                 return message.author.id == user.id
 
             try:
-                msg = await botobj.wait_for("message", check=check, timeout=45)
+                msg = await bot.wait_for("message", check=check, timeout=45)
+                print("1")
                 if msg.content.lower() == captcha["text"]:
                     return await dm.send('correct')
                 else:
@@ -71,10 +83,9 @@ class Captcha(commands.Cog):
 
             except asyncio.TimeoutError:
                 return await interaction.response.send_message('Timeout')
-
-
+        except Exception as e:
+            print(e)    
     
-
 
 def setup(bot):
     bot.add_cog(Captcha(bot))
